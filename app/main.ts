fbbe1635aadf0a32e72f16e637a86bcd1db8e949
vp <notejde@gmail.com>
2016-10-21 16:53:50 -0700

    add fixed spelling error at main.ts

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,6 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
 
-const platform = plaatformBrowserDynamic();
+const platform = platformBrowserDynamic();
 
 platform.bootstrapModule(AppModule);
