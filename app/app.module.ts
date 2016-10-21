1418da3e22d95156fb1f93e8ef28bb62c392c980
vp <notejde@gmail.com>
2016-10-20 17:43:11 -0700

    add *.json files & gulp file

---
+++ app/app.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser;
+import { Forms } from '@augular/forms';
+
+
+@NgModule({
+  imports: [
+    BrowserModule,
+
+  ],
+})
