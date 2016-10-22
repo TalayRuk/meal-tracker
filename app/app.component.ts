f907258ea5bae8d924865d92ac525df79c6a0ad6
vp <notejde@gmail.com>
2016-10-21 19:45:38 -0700

    add edit food to app

--- app/app.component.ts
+++ app/app.component.ts
@@ -22,15 +22,13 @@ import { Food } from './food.model';
           <h3>{{ currentFood.name }}</h3>
           <button (click)="showFood(currentFood)">Edit</button>
       </div>
-      <!-- /////// edit-food
-      <edit-food
-        [childSelectedFood]="selectedFood"
-        (doneClickedSender)="finishedEditing()"
-      ></edit-food> -->
-      <!-- add new food -->
       <new-food
         (newFoodSender)="addFood($event)"
       ></new-food> <!--from new-food.component.ts & also add trigger method called addFood that we'll declare next. addFood method's job will be to add the food to the array sending a food object along our newFoodSender bridge, we use the $event keyword to signify that our addFood method will be receiving an argument.-->
+      <edit-food
+        [childSelectedFood]="selectedFood"
+        (editedClickedSender)="finishedEditing()"
+      ></edit-food>
 
     </div>
   </div>
@@ -54,9 +52,13 @@ export class AppComponent {
   ];
   //show list
   selectedFood: Food = null;
-    showFood(clickedFood: Food) {
-      this.selectedFood = clickedFood;
-    }
+  showFood(clickedFood: Food) {
+    this.selectedFood = clickedFood;
+  }
+  //edit food
+  finishedEditing() {
+    this.selectedFood = null;
+  }  
   //Declare new food
   addFood(newFoodFromChild: Food) {
     this.masterFoodList.push(newFoodFromChild);
